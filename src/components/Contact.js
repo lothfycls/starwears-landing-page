62f48ffbbdb3882e96819f567f23b068a4294641
lothfy <lothfy.sabour@ext.leroymerlin.fr>
2023-02-20 00:04:00 +0100

    emailing with emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
 
     e.preventDefault();
 
-    emailjs.sendForm(process.envYOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
+    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
