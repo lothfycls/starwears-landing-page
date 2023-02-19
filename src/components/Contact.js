db6f8256712066b941076d979d4cc1562325eaca
lothfy <lothfy.sabour@ext.leroymerlin.fr>
2023-02-20 00:26:45 +0100

    .env

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
 
     e.preventDefault();
 
-    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
+    emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, form.current, process.env.REACT_APP_YOUR_PUBLIC_KEY)
       .then((result) => {
         console.log(process.env)
           console.log(result.text);
